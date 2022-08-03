9879ee66a89a7d97bf27cee08bddcb78e4ad138d
Tamim Mahmud <internationalinf24@gmail.com>
2022-08-03 22:30:31 +0600

    add example of object

--- app.js
+++ app.js
@@ -9,12 +9,23 @@ const person ={
         country : "Bangladesh"
     },
     birthYear : function(){
-        return 1992;
+        return 2022 - this.age;
     }
 }; 
 
 var info;
 info = person.address.city;
+//birth year count in JS
 info = person.birthYear();
 
 console.log(info);
+
+let people = [
+    { nam : "Rinku", age: 24},
+    { nam: "Pingku", age: 21},
+    { nam: "Jhingku", age: 22}
+]
+
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i].nam);
+}
